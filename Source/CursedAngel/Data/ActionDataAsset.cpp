8da15295f0cbb1ed63ca01a75b6662bfb5dd78d2
Nick <PriceNic165@outlook.com>
2026-04-02 23:06:09 -0500

    Update Git ignore and remove old code
    Update Git Ignore and remove Cursed Angel 5.5 source code

--- Source/CursedAngel/Data/ActionDataAsset.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Cursed Angel. All Rights Reserved.
-
-#include "ActionDataAsset.h"
-
-FPrimaryAssetId UActionDataAsset::GetPrimaryAssetId() const
-{
-	// Return asset type "Action" with this asset's name as the identifier
-	return FPrimaryAssetId(TEXT("Action"), GetFName());
-}
--- Source/CursedAngel/Data/CharacterDataAsset.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Data/CharacterDataAsset.h"
-
-// Empty implementation - this is a data-only class
-// All configuration is done through property editing in the Unreal Editor
